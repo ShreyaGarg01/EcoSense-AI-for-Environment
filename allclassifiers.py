bd4e0339d7d01ba7873356006f6a7e9774955c95
ShreyaGarg01 <shreyagarg0112@gmail.com>
2023-07-02 11:29:58 +0530

    Water

---
+++ allclassifiers.py
@@ -0,0 +1,57 @@
+from sklearn.datasets import make_circles, make_classification, make_moons
+# from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+# from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+# from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.inspection import DecisionBoundaryDisplay
+# from sklearn.model_selection import train_test_split
+# from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+
+# from sklearn.linear_model import SGDClassifier
+# from sklearn.naive_bayes import MultinomialNB
+# from lightgbm import LGBMClassifier
+# from xgboost.sklearn import XGBClassifier
+# from sklearn.tree import ExtraTreeClassifier
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.svm import OneClassSVM
+
+# from sklearn.neighbors import RadiusNeighborsClassifier
+# from sklearn.multioutput import ClassifierChain
+# from sklearn.multioutput import MultiOutputClassifier
+# from sklearn.multiclass import OutputCodeClassifier
+# from sklearn.multiclass import OneVsOneClassifier
+# from sklearn.multiclass import OneVsRestClassifier
+
+# from sklearn.linear_model import RidgeClassifierCV
+# from sklearn.linear_model import RidgeClassifier
+# from sklearn.linear_model import PassiveAggressiveClassifier    
+
+# from sklearn.ensemble import VotingClassifier
+
+# from sklearn.ensemble import BaggingClassifier
+# from sklearn.ensemble import ExtraTreesClassifier
+# from sklearn.naive_bayes import BernoulliNB
+# from sklearn.calibration import CalibratedClassifierCV
+# from sklearn.semi_supervised import LabelPropagation
+# from sklearn.semi_supervised import LabelSpreading
+# from sklearn.svm import LinearSVC
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.linear_model import LogisticRegressionCV
+# from sklearn.naive_bayes import MultinomialNB  
+# from sklearn.neighbors import NearestCentroid
+# from sklearn.svm import NuSVC
+# from sklearn.linear_model import Perceptron
+# from sklearn.svm import SVC
+# # from sklearn.mixture import DPGMM
+# # from sklearn.mixture import GMM 
+# from sklearn.mixture import GaussianMixture
+# # from sklearn.mixture import VBGMM
+
+from sklearn.neighbors import KNeighborsClassifier
